4f687ec0810c5f730768bef211394e2a97fd3df0
Moaz Talaat <88838071+MoazSalem@users.noreply.github.com>
2025-04-22 19:41:18 +0200

    downgrade file_picker for now
    there is a path duplication bug in the latest version

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Photo Sorter',
+      title: 'Image Sorter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
         useMaterial3: true,
@@ -51,7 +51,7 @@ class _PhotoSorterHomeState extends State<PhotoSorterHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Photo Sorter')),
+      appBar: AppBar(title: const Text('Image Sorter')),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: SingleChildScrollView(
